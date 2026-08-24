164cfa62d073af8abacdad46b81f720dd69213c3
Nick6936 <nikhilparajuli31@gmail.com>
2026-08-24 20:31:07 +0545

    fix: Login requiring dashboard URL

--- pages/auth/LoginPage.ts
+++ pages/auth/LoginPage.ts
@@ -28,6 +28,12 @@ export class LoginPage {
     await this.usernameInput.fill(username);
     await this.passwordInput.fill(password);
     await this.loginButton.click();
+  }
+
+  async loginAndWaitForDashboard(username: string, password: string) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
     await this.page.waitForURL(/dashboard/);
   }
 
